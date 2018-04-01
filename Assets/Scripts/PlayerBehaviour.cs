5b097cf395c7c99e72bcc79ee56a2447628b975f
Tanukode <nicolas.huerta@gmail.com>
2018-04-01 00:10:42 -0300

    1st commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveHorizontal ();
+	}
+
+	void MoveHorizontal(){
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 3.0f;
+		transform.Translate (x, 0, 0);
+	}
+
+	void Jump(){
+		
+	}
+}
+
